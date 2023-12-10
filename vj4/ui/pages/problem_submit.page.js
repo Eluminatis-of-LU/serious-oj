dc6e7699271e600246d7f22bc48f3c35bbc8ec3d
Araf Al Jami <clown1331@gmail.com>
2023-12-10 01:20:20 +0000

    Update timer on problem submit;

--- vj4/ui/pages/problem_submit.page.js
+++ vj4/ui/pages/problem_submit.page.js
@@ -1,7 +1,7 @@
 import { NamedPage } from 'vj/misc/PageLoader';
 import CountdownTimer from 'vj/components/countdown-timer';
 
-const page = new NamedPage('problem_submit', async () => {
+const page = new NamedPage(['problem_submit', 'contest_detail_problem_submit', 'homework_detail_problem_submit'], async () => {
   CountdownTimer.getOrConstruct($('#contest-countdown-timer'));
   $(document).on('click', '[name="problem-sidebar__show-category"]', ev => {
     $(ev.currentTarget).hide();
