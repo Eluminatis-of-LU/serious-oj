e56e73326afbbe8fd807416020fbc109a8fc12ae
Araf Al Jami <clown1331@gmail.com>
2023-12-13 22:00:49 +0000

    Update aiosmtplib

--- vj4/service/mailer.py
+++ vj4/service/mailer.py
@@ -22,9 +22,9 @@ async def send_mail(to: str, subject: str, content: str):
   msg['Subject'] = subject
   msg['From'] = options.mail_from
   msg['To'] = to
-
+  
   try:
-    async with aiosmtplib.SMTP_SSL(hostname=options.smtp_host, port=options.smtp_port) as server:
+    async with aiosmtplib.SMTP(hostname=options.smtp_host, port=options.smtp_port) as server:
       await server.ehlo()
       await server.login(options.smtp_user, options.smtp_password)
       await server.sendmail(options.mail_from, to, msg.as_string())
