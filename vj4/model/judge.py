94da2692bb87f42bd9d1efdb9147caec50a34bd4
Araf Al Jami <clown1331@yahoo.com>
2025-01-10 22:41:49 +0000

    Update index

--- vj4/model/judge.py
+++ vj4/model/judge.py
@@ -3,16 +3,19 @@ from vj4 import error
 from vj4.util import argmethod
 import datetime
 
-async def insert_judge(doc):
-  coll = db.coll('judge')
-  doc['created_at'] = datetime.datetime.utcnow()
-  await coll.insert_one(doc)
+
+async def checkin(doc):
+    coll = db.coll("judge")
+    expire_at = datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+    doc["expire_at"] = expire_at
+    await coll.insert_one(doc)
+
 
 @argmethod.wrap
 async def ensure_indexes():
-  coll = db.coll('judge')
-  await coll.create_index([('created_at', 1)], expireAfterSeconds=60)
+    coll = db.coll("judge")
+    await coll.create_index("expire_at", expireAfterSeconds=0)
 
 
-if __name__ == '__main__':
-  argmethod.invoke_by_args()
+if __name__ == "__main__":
+    argmethod.invoke_by_args()
